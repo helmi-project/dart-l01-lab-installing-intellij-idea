bdbde35246ef98a5f44ed84422691d9fe477c1f2
helmibelogix <helmi@belogix.com>
2020-11-05 13:21:35 +0700

    First Commit

---
+++ bin/l01_lab_installing_intellij_idea.dart
@@ -0,0 +1,5 @@
+import 'package:l01_lab_installing_intellij_idea/l01_lab_installing_intellij_idea.dart' as l01_lab_installing_intellij_idea;
+
+void main(List<String> arguments) {
+  print('Hello world: ${l01_lab_installing_intellij_idea.calculate()}!');
+}
---
+++ test/l01_lab_installing_intellij_idea_test.dart
@@ -0,0 +1,8 @@
+import 'package:l01_lab_installing_intellij_idea/l01_lab_installing_intellij_idea.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
